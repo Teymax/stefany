cc55ff5e6577626b0e395fb4d0d2404fe7fc7df2
htnpro <he4then.mail@gmail.com>
2019-12-06 11:47:47 +0200

    new layout

---
+++ homepage/src/js/main.js
@@ -0,0 +1,10 @@
+// ( () => {
+//   const baba1 = document.querySelector( '.baba-1' )
+//   console.log( baba1 )
+//   const appendHandlers = baba => {
+//     baba.addEventListener( 'click', event => {
+//       console.log( event.target )
+//     } )
+//     baba.addEventListener('')
+//   }
+// } )()
