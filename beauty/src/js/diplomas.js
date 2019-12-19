c1af8160881d32993b7620896dff33e341f230ad
alexey <alexeysobolev071@gmail.com>
2019-12-19 15:52:25 +0200

    hot fix

--- beauty/src/js/diplomas.js
+++ beauty/src/js/diplomas.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
-
-  document.re
   let diploma = ''
   for (let i = 0; i < 62; i++) {
-    console.log(i);
     diploma += `
         <a class="diploma-item" href="./assets/img/diplomas/modal/${i}.jpg" data-fancybox="">
           <img class="w-100" src="./assets/img/diplomas/${i}.jpg" alt="diploma"/>
@@ -11,5 +8,4 @@ $(document).ready(function() {
       `;
   }
   document.querySelector('.diploma-slider').innerHTML = diploma
-
 });
