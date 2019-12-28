d9576f2dd868369372dba7be7a372bee78e908f2
htnpro <he4then.mail@gmail.com>
2019-12-28 14:32:05 +0200

    missed i18n switcher prop

--- beauty/src/js/i18n.js
+++ beauty/src/js/i18n.js
@@ -18,7 +18,8 @@ $(document).ready(function () {
     const p = location.pathname.split('/').slice(-1)[0]
     const urlCity      = location.pathname.slice(1).split('/')[1],
           filteredCity = ['rv'].find(city => city === urlCity) || 'zt'
-    const h = window.patterns[i18n].replace('{{ city }}', filteredCity).replace('$1', p)
+    const h = filteredCity !== 'zt' ? window.patterns[i18n].replace('{{ city }}', filteredCity).replace('$1',
+      p) : window.patterns[i18n].replace('/{{ city }}', '').replace('$1', p)
     if (location.pathname.substr(1) !== h) {
       location.href = '/' + h
     }
