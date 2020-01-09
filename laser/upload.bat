93fdcecffad0176fb6ffd647b3cb9f2f1193b032
alexey <alexeysobolev071@gmail.com>
2020-01-09 17:05:45 +0200

    change breadcrambs

--- laser/upload.bat
+++ laser/upload.bat
@@ -2,11 +2,12 @@ set ERRORLEVEL=1
 mkdir uploadBuild
 mkdir uploadBuild\ua
 cd lib\gulp
-gulp build
+start gulp build
 echo 'Done'
 cd ..\..\ua\lib\gulp
-gulp build
+start gulp build
 cd ..\..\
-move build\** ..\uploadBuild\ua
+move build\ ..\uploadBuild\ua
 cd ..\
-move build\** uploadBuild\
+move build\ uploadBuild\
+rem taskkill /IM "cmd.exe" /F
