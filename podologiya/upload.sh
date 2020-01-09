b101a945a1fe3d6c3b7cdb6c657cca22cb7c2db7
htnpro <he4then.mail@gmail.com>
2020-01-09 17:38:39 +0200

    hot fix

--- podologiya/upload.sh
+++ podologiya/upload.sh
@@ -1,10 +1,10 @@
+rm -rf uploadBuild
 mkdir uploadBuild && mkdir uploadBuild/ua
 cd lib/gulp
 gulp build
 cd ../../ua/lib/gulp
 gulp build
 cd ../../
-mkdir ../uploadBuild/ua
 mv build/** ../uploadBuild/ua
 cd ../
 mv build/** uploadBuild/
