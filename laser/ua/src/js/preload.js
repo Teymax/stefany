fe97d4ccce32664f6341ac080e377ee9ee4b4177
alexey <alexeysobolev071@gmail.com>
2020-01-09 13:38:41 +0200

    reload js add pages

--- laser/ua/src/js/preload.js
+++ laser/ua/src/js/preload.js
@@ -3,35 +3,46 @@ function preloadFunc() {
     "rv": [
       "epilation",
       "nano-epilyatsiya",
-      "shugaring"
+      "shugaring",
+      "about",
+      "salon",
+      "contact"
     ],
     "lt": [
       "epilation",
-      "nano-epilyatsiya"
+      "nano-epilyatsiya",
+      "about",
+      "salon",
+      "contact"
     ],
     "if": [
-      "epilation"
+      "epilation",
+      "about",
+      "salon",
+      "contact"
     ],
     "lv": [
-      "epilation"
+      "epilation",
+      "about",
+      "salon",
+      "contact"
     ]
   };
   let path = location.pathname.split("/");
   let pathArr = path.slice(2);
   console.log(pathArr);
-  if (pathArr.length > 1 && links[pathArr[0]] && pathArr[1] !== "" && pathArr[1] !== "ua" && !links[pathArr[0]].includes(pathArr[1]))
-  {
-    let a = path.slice(0,-1).join("/");
+  if (pathArr.length > 1 && links[pathArr[0]] && pathArr[1] !== "" && pathArr[1] !== "ua" && !links[pathArr[0]].includes(pathArr[1])) {
+    let a = path.slice(0, -1).join("/");
     console.log(a);
-    window.location.href = a+"/";
+    window.location.href = a + "/";
   }
-  if (pathArr.length > 1 && links[pathArr[0]] && pathArr[1] !== "" && pathArr[1] === "ua" && pathArr[2] !== "" && !links[pathArr[0]].includes(pathArr[2]))
-  {
+  if (pathArr.length > 1 && links[pathArr[0]] && pathArr[1] !== "" && pathArr[1] === "ua" && pathArr[2] !== "" && !links[pathArr[0]].includes(pathArr[2])) {
     console.log("2");
-    let a = path.slice(0,-1).join("/");
+    let a = path.slice(0, -1).join("/");
     console.log(a);
 
-    window.location.href = a+"/";
+    window.location.href = a + "/";
   }
 }
+
 window.onpaint = preloadFunc();
