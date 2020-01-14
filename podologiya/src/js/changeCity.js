6f720a82dc5e686f2f20e4b5cd7daaa812c84b6f
htnpro <he4then.mail@gmail.com>
2020-01-14 13:22:01 +0200

    todo town switch

--- podologiya/src/js/changeCity.js
+++ podologiya/src/js/changeCity.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
         cityMap               : '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6288.768477360866!2d28.67355334502534!3d50.25248404598579!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe2cbe153518a079e!2sSteffany!5e0!3m2!1sru!2sua!4v1578986511431!5m2!1sru!2sua" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>',
         cityInstagram         : 'https://www.instagram.com/steffany.podology/?hl=ru',
         cityFacebook          : 'https://www.facebook.com/steffany.podology/',
-        imageAboutStaffanyPage: 'assets/img/about-steffany-salon/zt/{{ salon }}/salon-0.jpg',
+        imageAboutStaffanyPage: 'assets/img/about-steffany-salon/zt/{{ salon }}/salon-0.png',
         
         imagesAmount: 10,
         
@@ -27,79 +27,15 @@ $(document).ready(function () {
             facebook   : 'https://www.facebook.com/steffany.podology/',
             specialists: [
               {
-                name      : 'Алина Кайсын',
-                education : 'Мастер ногтевого сервиса',
-                experience: 'Работает уже 7 лет',
-                skills    : 'Сделает все виды маникюра и педикюра, выравнивание и укрепление ногтевой пластины, наращивание ногтей, дизайн (геометрию, аэрографию, френч, стемпинг, несложные рисунки, втирку), парафинотерапию для рук и ног, спа-процедуру CND',
-                cover     : ''
+                name      : 'Руслана Даценко',
+                education : 'Подолог, специалист по ортониксии, преподаватель и автор обучающих методик',
+                skills    : 'Специализируется на аппаратном медицинском педикюре, компьютерной диагностике стопы и биомеханики ходьбы, проектировании индивидуальных стелек, изготовлении разгрузочных стелек PWO'
               },
               {
-                name      : 'Наталия Клец',
-                education : 'Мастер ногтевого сервиса',
-                experience: 'Работает уже 1 год',
-                skills    : 'Сделает все виды маникюра и педикюра, выравнивание, укрепление, наращивание и коррекцию ногтей, несложный дизайн, парафинотерапию для рук и ног'
+                name      : 'Руслана Даценко',
+                education : 'Подолог, специалист по ортониксии, преподаватель и автор обучающих методик',
+                skills    : 'Специализируется на аппаратном медицинском педикюре, компьютерной диагностике стопы и биомеханики ходьбы, проектировании индивидуальных стелек, изготовлении разгрузочных стелек PWO'
               },
-              {
-                name      : 'Людмила Коржовская',
-                education : 'Мастер ногтевого сервиса',
-                experience: 'Работает уже 7 ле',
-                skills    : 'Сделает все виды маникюра и педикюра, выравнивание и укрепление ногтевой пластины, дизайн (геометрию, аэрографию, френч, стемпинг, несложные рисунки, втирку), парафинотерапию для рук и ног, спа-процедуру CND'
-              },
-              {
-                name      : 'Владлена Охмак',
-                education : 'Мастер ногтевого сервиса',
-                experience: 'Работает уже 5 лет',
-                skills    : 'Сделает европейский, классический, мужской, детский маникюр, выравнивание, укрепление и «ремонт» ногтевой пластины, все виды дизайна, классический, кислотный и спа-педикюр, парафинотерапию для рук и ног'
-              },
-              
-              // {
-              //   name      : 'Ольга Петрук',
-              //   education : 'Мастер ногтевого сервиса',
-              //   experience: 'Работает уже 1 год',
-              //   skills    : 'Сделает все виды маникюра, выравнивание и укрепление ногтевой пластины, классический, кислотный и спа-педикюр, дизайн (френч, аэрографию, геометрию, втирку, наклейки, несложные рисунки)'
-              // },
-              {
-                name      : 'Анна Роюк',
-                education : 'Мастер ногтевого сервиса',
-                experience: 'Работает уже 2 года',
-                skills    : 'Сделает все виды маникюра, выравнивание, укрепление и наращивание ногтей, классический, кислотный и комбинированный педикюр, несложный дизайн, спа-процедуры и парафинотерапию для рук и ног'
-              },
-              {
-                name      : 'Ксения Станиславская',
-                education : 'Директор Nail Studio Steffany, мастер ногтевого сервиса',
-                experience: 'Работает уже 13 лет',
-                skills    : 'Сделает все виды маникюра и педикюра, выравнивание, наращивание и коррекцию ногтей, все виды дизайна, парафинотерапию для рук и ног'
-              },
-              {
-                name      : 'Ольга Степанчук',
-                education : 'Мастер ногтевого сервиса',
-                experience: 'Работает уже 6 лет',
-                skills    : 'Сделает классический, комбинированный и аппаратный маникюр, классический, комбинированный и кислотный педикюр, парафинотерапию для рук и ног, укрепление, «ремонт» и наращивание ногтей, все виды нехудожественного дизайна'
-              },
-              {
-                name      : 'Анастасия Грицык',
-                education : 'Мастер ногтевого сервиса',
-                experience: 'Работает уже 4 года',
-                skills    : 'Сделает все виды маникюра и педикюра, выравнивание и укрепление ногтевой пластины, наращивание и коррекцию ногтей, несложный дизайн ногтей'
-              },
-              {
-                name      : 'Ирина Демиденко',
-                education : 'Мастер ногтевого сервиса',
-                experience: 'Работает уже 4 года',
-                skills    : 'Сделает все виды маникюра и педикюра, выравнивание и укрепление ногтевой пластины, наращивание ногтей, все виды дизайна ногтей, парафинотерапию для рук и ног'
-              },
-              {
-                name      : 'Мария Пашковская',
-                education : 'Мастер ногтевого сервиса',
-                experience: 'Работает уже 2 года',
-                skills    : 'Сделает все виды маникюра и педикюра, выравнивание и укрепление ногтевой пластины, наращивание и коррекцию ногтей, дизайн (аэрографию, втирку, стемпинг), парафинотерапию для рук и ног'
-              },
-              {
-                name      : 'Оксана Таргонская',
-                education : 'Мастер ногтевого сервиса',
-                experience: 'Работает уже 3 года',
-                skills    : 'Сделает все виды маникюра, выравнивание и укрепление ногтевой'
-              }
             ]
           }
         }
@@ -113,7 +49,7 @@ $(document).ready(function () {
         cityMap               : '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6288.768477360866!2d28.67355334502534!3d50.25248404598579!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe2cbe153518a079e!2sSteffany!5e0!3m2!1sru!2sua!4v1578986511431!5m2!1sru!2sua" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>',
         cityInstagram         : 'https://www.instagram.com/steffany.podology/?hl=ru',
         cityFacebook          : 'https://www.facebook.com/steffany.podology/',
-        imageAboutStaffanyPage: 'assets/img/about-steffany-salon/zt/{{ salon }}/salon-0.jpg',
+        imageAboutStaffanyPage: 'assets/img/about-steffany-salon/zt/{{ salon }}/salon-0.png',
         
         imagesAmount: 10,
         
@@ -218,7 +154,7 @@ $(document).ready(function () {
         cityMap               : '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d532.9732425877958!2d26.269108057641883!3d50.6165984522853!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x472f1366b7f21ce1%3A0x6d5a726bdbefb70!2z0YPQuy4g0JrQuNC10LLRgdC60LDRjywgNCwg0KDQvtCy0L3Qviwg0KDQvtCy0LXQvdGB0LrQsNGPINC-0LHQu9Cw0YHRgtGMLCAzMzAwMA!5e0!3m2!1sru!2sua!4v1578987175913!5m2!1sru!2sua" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>',
         cityInstagram         : 'https://www.instagram.com/steffany.rivne/?hl=ru',
         cityFacebook          : 'https://www.facebook.com/steffany.ua/',
-        imageAboutStaffanyPage: 'assets/img/about-steffany-salon/rv/{{ salon }}/salon-0.jpg',
+        imageAboutStaffanyPage: 'assets/img/about-steffany-salon/rv/{{ salon }}/salon-0.png',
         
         imagesAmount: 6,
         salons      : {
@@ -320,7 +256,7 @@ $(document).ready(function () {
         cityMap               : '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d532.9732425877958!2d26.269108057641883!3d50.6165984522853!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x472f1366b7f21ce1%3A0x6d5a726bdbefb70!2z0YPQuy4g0JrQuNC10LLRgdC60LDRjywgNCwg0KDQvtCy0L3Qviwg0KDQvtCy0LXQvdGB0LrQsNGPINC-0LHQu9Cw0YHRgtGMLCAzMzAwMA!5e0!3m2!1sru!2sua!4v1578987175913!5m2!1sru!2sua" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>',
         cityInstagram         : 'https://www.instagram.com/steffany.rivne/?hl=ru',
         cityFacebook          : 'https://www.facebook.com/steffany.ua/',
-        imageAboutStaffanyPage: 'assets/img/about-steffany-salon/rv/{{ salon }}/salon-0.jpg',
+        imageAboutStaffanyPage: 'assets/img/about-steffany-salon/rv/{{ salon }}/salon-0.png',
         
         imagesAmount: 6,
         salons      : {
@@ -649,7 +585,7 @@ $(document).ready(function () {
     for (let i = 0; i < imagesAmount; i++) {
       images += `
         <span data-pos="${i}" class="video d-flex align-items-center justify-content-center">
-          <img src="${imgPath}/${salon}/salon-${i}.jpg" class="w-100">
+          <img src="${imgPath}/${salon}/salon-${i}.png" class="w-100">
         </span>
       `
     }
@@ -657,7 +593,7 @@ $(document).ready(function () {
     for (let i = 0; i < imagesAmount; i++) {
       imagesSmall += `
         <span data-pos="${i}" class="owl-dot d-flex align-items-center justify-content-center mb-4">
-          <img src="${imgPath}/${salon}/salon-${i}.jpg" class="w-100 salon-img">
+          <img src="${imgPath}/${salon}/salon-${i}.png" class="w-100 salon-img">
           <span class="owl-dot-bg w-100 h-100 d-block"></span>
         </span>
       `
@@ -666,7 +602,7 @@ $(document).ready(function () {
       imagesSpecialists += `
         <div class="specialists-item">
             <div class="specialists-item-container mb-3 w-100">
-                <div class="specialists-img w-100"><img class="w-100" src="${imgPath}/specialists/${salon}/specialist-${i}.jpg" alt="${specialistsData[i].name}"/></div>
+                <div class="specialists-img w-100"><img class="w-100" src="${imgPath}/specialists/${salon}/specialist-${i}.png" alt="${specialistsData[i].name}"/></div>
                 <div class="specialists-info d-flex flex-column align-items-start w-100 p-3">
                     <p class="paragraph-text text-color-lightdark text-w-light">${specialistsData[i].education}</p>
                     <p class="paragraph-text text-color-lightdark text-w-bold">${specialistsData[i].experience}</p>
