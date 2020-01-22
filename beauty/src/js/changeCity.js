854c1923875b8f3f6ed7a0be92dc347b26c1e1db
alexey <alexeysobolev071@gmail.com>
2020-01-22 10:18:39 +0200

    hot fix

--- beauty/src/js/changeCity.js
+++ beauty/src/js/changeCity.js
@@ -19,7 +19,7 @@ $(document).ready(function () {
             name       : 'Steffany Nail',
             city       : 'Житомир',
             nameIn     : 'в Житомире',
-            phone      : '380-67-404-19-33',
+            phone      : '380-98-180-50-90',
             address    : 'ул. Киевская, 77, тц "Глобал"',
             schedule   : '9:00 - 21:00',
             map        : '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2550.3732072342173!2d28.683759515257734!3d50.26628980849905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x697c4ee100bcc4de!2sSteffany%20Nail%20Studio!5e0!3m2!1sru!2sua!4v1578658568743!5m2!1sru!2sua" width="600" height="100%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>',
